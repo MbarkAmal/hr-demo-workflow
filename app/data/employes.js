d162448f64471874f1a46f2a03d6fe6360100d92
MbarkAmal <150669674+MbarkAmal@users.noreply.github.com>
2025-11-17 15:07:00 +0100

    AGR-12: AJOuter donnees saisies

---
+++ app/data/employes.js
@@ -0,0 +1,12 @@
+export const employes =[
+    {
+        fullName :"amal mbark",
+        email:"amal@gmail.com",
+        password:"amal123"
+    },
+      {
+        fullName :"maram mbark",
+        email:"maram@gmail.com",
+        password:"amal123"
+    },
+] 
